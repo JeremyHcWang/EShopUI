4d4a0b02d5a1379fe495babbf83680b44b979b3c
JeremyHcWang <jemhw9758@gmail.com>
2024-04-30 11:56:00 -0400

    update product request

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -2,6 +2,7 @@ import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../model/Product';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,15 @@ export class ProductsService {
 
   getAllProducts():Observable<Product[]> {
     //let headers = new HttpHeaders();
-    return this.http.get<Product[]>("https://productapp.jollystone-4c42e2d6.eastus2.azurecontainerapps.io/api/product");
+    return this.http.get<any[]>("https://productapp.jollystone-4c42e2d6.eastus2.azurecontainerapps.io/api/product").pipe(
+      map(
+        data => data.map(item => ({
+          id: item.productId,
+          name: item.productName,
+          price: item.unitPrice,
+          stock: item.unitsInStock
+        } as Product))
+      )
+    );
   }
 }
